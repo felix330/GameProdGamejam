047b48a8a90588614341bc368d5d5c3d280d179e
unknown <elke.scherffius@hs-furtwangen.de>
2017-07-10 14:30:43 +0200

    Kopfsteuerung hinzugefügt

---
+++ Assets/Scripts/BodyBehaviourPlaceholder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyBehaviourPlaceholder : MonoBehaviour {
+	
+	public static bool isFocused = false;
+	private float bodyRotation;
+	private float rotationSpeed = 150;
+	private float bodyMovement;
+	private float movementSpeed = 1;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		bodyRotation = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+		bodyMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		
+		//Wenn der Kopf nicht auf einem Körper plaziert ist und den Bodenberührt, 
+		//kann der Spieler ihn über A und D Rotieren. Mit F kann er einen Körper anvisieren und danach diesen aus SecondPerson-Sicht steuern
+		if(isFocused){
+			transform.Rotate(Vector3.up * bodyRotation);
+			transform.Translate(Vector3.forward * bodyMovement);
+		}
+	}
+	
+	void OnCollisionEnter(Collision collisionInfo){
+		if(collisionInfo.gameObject.name == "Head"){
+			collisionInfo.gameObject.transform.parent = this.gameObject.transform;
+			collisionInfo.gameObject.transform.rotation = this.gameObject.transform.rotation;
+		}
+	}
+}
---
+++ Assets/Scripts/HeadBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBehaviour : MonoBehaviour {
+	public bool groundTouching = false;
+	public bool isFocusedOnBody = false;
+	
+	private float headRotation;
+	private float rotationSpeed = 180;
+	private Vector3 rayForFocusingOnBody;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		headRotation = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+		
+		if(!isFocusedOnBody && groundTouching){
+			transform.Rotate(Vector3.up * headRotation);
+			if(Input.GetKey(KeyCode.F)){
+				FocusOnBody();
+			}
+		}
+	}
+	
+	void OnCollisionStay(Collision collisionInfo){
+		if(collisionInfo.gameObject.name == "Ground"){
+			groundTouching = true;
+		}
+	}
+	
+	void FocusOnBody(){
+		rayForFocusingOnBody = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		
+		//Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, rayForFocusingOnBody * 5, Color.green);
+
+        if (Physics.Raycast(transform.position, rayForFocusingOnBody, out hit, 5)){
+			if(hit.transform.tag == "Body"){
+				Debug.Log("Körper entdeckt");
+				isFocusedOnBody = true;
+				BodyBehaviourPlaceholder.isFocused = true;
+			}
+		}
+	}
+		
+		
+}
