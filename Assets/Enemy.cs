82c6cb0b52c5a092efc0f7d7473b5808c2dbbcc6
Felix Gähr <felix330@gmail.com>
2017-07-11 14:28:27 +0200

    Enemy Start

---
+++ Assets/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	private GameObject head;
+
+	private Vector3 basePosition;
+	private Quaternion baseRotation;
+
+	private CharacterController controller;
+	public float gravity;
+
+	private bool inPursuit;
+	// Use this for initialization
+	void Start () {
+		basePosition = transform.position;
+		baseRotation = transform.rotation;
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Only follow stray heads
+		Vector3 moveDirection = new Vector3();
+		if (head != null && !head.GetComponent<HeadBehaviour>().attachedToBody && head.GetComponent<HeadBehaviour>().groundTouching)
+		{
+			moveDirection = head.transform.position;
+		} else {
+			moveDirection = basePosition;
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	//message receivers
+	void HeadFound (GameObject h) {
+		Debug.Log("Found a head");
+		head = h;
+		inPursuit = true;
+	}
+
+	void HeadGone (GameObject h) {
+		Debug.Log("Head is gone!");
+	}
+}
---
+++ Assets/EnemyTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider c)
+	{
+		if (c.gameObject.tag == "Head")
+		{
+			transform.parent.SendMessage("HeadFound",c.gameObject);
+		}
+	}
+
+	void OnTriggerExit (Collider c)
+	{
+		if (c.gameObject.tag == "Head")
+		{
+			transform.parent.SendMessage("HeadGone",c.gameObject);
+		}
+	}
+}
