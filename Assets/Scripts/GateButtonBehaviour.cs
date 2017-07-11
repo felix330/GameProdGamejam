5fa9d978a4041bca629b3733f204e2b37e3b387e
unknown <elke.scherffius@hs-furtwangen.de>
2017-07-11 12:28:46 +0200

    Tor- und Schlüssel-Funktion eingebaut

---
+++ Assets/Scripts/GateButtonBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateButtonBehaviour : MonoBehaviour {
+	public List<string> gateKeys = new List<string>();
+	public List<GameObject> gates = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter(Collision collisionInfo){
+		if(gateKeys.Contains(collisionInfo.gameObject.name)){
+			//Debug.Log(collisionInfo.gameObject.name);
+			foreach(string key in gateKeys){
+				foreach(GameObject gate in gates){
+					Debug.Log(key + " + " + gate);
+					gate.GetComponent<GateBehaviour>().SendMessage("OpenGate", key);
+				}
+			}
+		}
+	}
+}
