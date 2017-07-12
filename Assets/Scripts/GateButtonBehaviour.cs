cabeeabe5c6f623eee4508a863c080b33f7cec36
unknown <elke.scherffius@hs-furtwangen.de>
2017-07-12 20:41:56 +0200

    Kleine Änderungen in den Prefabs und dem Werfen

--- Assets/Scripts/GateButtonBehaviour.cs
+++ Assets/Scripts/GateButtonBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GateButtonBehaviour : MonoBehaviour {
-	public List<string> gateKeys = new List<string>();
+	public List<GameObject> gateKeys = new List<GameObject>();
 	public List<GameObject> gates = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
@@ -16,9 +16,9 @@ public class GateButtonBehaviour : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter(Collision collisionInfo){
-		if(gateKeys.Contains(collisionInfo.gameObject.name)){
+		if(gateKeys.Contains(collisionInfo.gameObject)){
 			//Debug.Log(collisionInfo.gameObject.name);
-			foreach(string key in gateKeys){
+			foreach(GameObject key in gateKeys){
 				foreach(GameObject gate in gates){
 					//Debug.Log(key + " + " + gate);
 					gate.GetComponent<GateBehaviour>().SendMessage("OpenGate", key);
