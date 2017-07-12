fe1c9a602f4879665e282077775ef2af3bdf460a
Felix Gähr <felix330@gmail.com>
2017-07-12 20:02:35 +0200

    Added indicator pyramid

---
+++ Assets/Indicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Shows a possessed body
+public class Indicator : MonoBehaviour {
+
+	private Body b;
+	// Use this for initialization
+	void Start () {
+		b = transform.parent.gameObject.GetComponent<Body>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (b.headless && b.isUsed){
+			GetComponent<MeshRenderer>().enabled = true;
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,transform.localEulerAngles.y+1,transform.localEulerAngles.z);
+		} else {
+			GetComponent<MeshRenderer>().enabled = false;
+		}
+	}
+}
